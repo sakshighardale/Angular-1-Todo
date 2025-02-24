624b9b6f2b81f32e1df92790a709181cefcaf64c
sakshighardale <142718912+sakshighardale@users.noreply.github.com>
2025-02-25 00:22:10 +0530

    Updated README with images

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+#Images
+
+# My Project
+
+This is a sample project.
+
+## Screenshots
+
+### Screenshot 1
+
+![Screenshot 1](assets/Image1.png)
+
+### Screenshot 2
+
+![Screenshot 2](assets/Image2.png)
+
+### Screenshot 3
+
+![Screenshot 3](assets/Image3.png)
+
 # Todo
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.6.
@@ -57,4 +77,4 @@ Angular CLI does not come with an end-to-end testing framework by default. You c
 ## Additional Resources
 
 For more information on using the Angular CLI, including detailed command references, visit the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
-"# Angular-1-Todo" 
+"# Angular-1-Todo"
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { NgClass, NgFor } from '@angular/common';
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, NgFor, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Todo';
+  todoList: TodoItem[] = [];
+  newTask: string = '';
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false,
+      };
+      this.todoList.push(newTodoItem);
+      this.newTask = '';
+      // console.log(this.todoList);
+    }
+  }
+
+  toggleComplete(index: number): void {
+    console.log(index);
+    this.todoList[index].completed = !this.todoList[index].completed;
+    console.log(this.todoList);
+  }
+
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter((item) => item.id != id);
+    console.log(this.todoList);
+  }
 }
